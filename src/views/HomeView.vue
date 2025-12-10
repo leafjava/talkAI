3b8b902440758c490be83e1dc9dc387cabc28f81
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-12-10 23:55:45 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+  <iframe
+  src="https://udify.app/chatbot/IYkwDNgqfhMHogeV"
+  style="width: 100%; height: 100%; min-height: 700px"
+  frameborder="0"
+  allow="microphone">
+  </iframe>
+</template>
